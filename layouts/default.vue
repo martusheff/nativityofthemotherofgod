37f9dbb5bcd9458798cbdffcc10bc8f30e5481fc
Andronick Martusheff <andronick@outlook.com>
2025-07-10 10:26:50 -0800

    Add Layouts and Pages Directories

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="layout">
+    <!-- <Header /> -->
+    <main>
+      <slot />
+    </main>
+    <!-- <Footer /> -->
+  </div>
+</template>
+
+<script setup>
+
+definePageMeta({
+  layout: 'default'
+})
+</script>
+
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <section>
+        <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </section>
+</template>
+
+<style scoped>
+
+</style>
