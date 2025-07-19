07123e4d6f9128efff7c97d5bd7baeb17a016105
Andronick Martusheff <andronick@outlook.com>
2025-07-19 00:16:20 -0800

    Nuxt 4  Cleanup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 // @ts-check
+// @ts-ignore
 import withNuxt from '.nuxt/eslint.config.mjs'
 
 export default withNuxt(
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -48,11 +48,9 @@ export default defineNuxtConfig({
   pwa: {
     registerType: 'autoUpdate',
     manifest: {
-      name: 'Nativity of the Mother of God',
-      short_name: 'NMOG',
+      name: 'Church of the Nativity of the Mother of God',
+      short_name: 'Nativity of the Mother of God',
       description: 'ROCOR Old Rite Russian Orthodox Church in Woodburn, OR',
-
-
 
       theme_color: '#ffffff',
       background_color: '#ffffff',
@@ -61,16 +59,56 @@ export default defineNuxtConfig({
       start_url: '/',
       icons: [
         {
-          src: '/favicon.ico',
-          sizes: '192x192',
-          type: 'image/png'
+          "src": "icons/icon-48x48.png",
+          "sizes": "48x48",
+          "type": "image/png"
         },
         {
-          src: '/favicon.ico',
-          sizes: '512x512',
-          type: 'image/png'
+          "src": "icons/icon-72x72.png",
+          "sizes": "72x72",
+          "type": "image/png"
+        },
+        {
+          "src": "icons/icon-96x96.png",
+          "sizes": "96x96",
+          "type": "image/png"
+        },
+        {
+          "src": "icons/icon-128x128.png",
+          "sizes": "128x128",
+          "type": "image/png"
+        },
+        {
+          "src": "icons/icon-144x144.png",
+          "sizes": "144x144",
+          "type": "image/png"
+        },
+        {
+          "src": "icons/icon-152x152.png",
+          "sizes": "152x152",
+          "type": "image/png"
+        },
+        {
+          "src": "icons/icon-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png"
+        },
+        {
+          "src": "icons/icon-256x256.png",
+          "sizes": "256x256",
+          "type": "image/png"
+        },
+        {
+          "src": "icons/icon-384x384.png",
+          "sizes": "384x384",
+          "type": "image/png"
+        },
+        {
+          "src": "icons/icon-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png"
         }
-      ]
+      ],
     },
     workbox: {
       navigateFallback: '/',
